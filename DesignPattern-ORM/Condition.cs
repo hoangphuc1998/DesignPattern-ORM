05c034590898dcd2f91a29a54ebc156ad48c182d
Trang Trung Hoàng Phúc <trangtrunghoangphuc@gmail.com>
2019-12-23 23:42:05 +0700

    Create Condition.cs

---
+++ DesignPattern-ORM/Condition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_ORM
+{
+    abstract class Condition
+    {
+        // protected bool isNot = false;
+        public abstract string toSQL();
+        /**
+         USAGE
+        
+         * */
+    }
+}
