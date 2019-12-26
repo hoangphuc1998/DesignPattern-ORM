0d30cbe1304c52dfecca85bf5da9aec70f235eb7
minh.to <minh.to@eyeq.tech>
2019-12-26 22:54:42 +0700

    f

--- DesignPattern-ORM/Condition/Condition.cs
+++ DesignPattern-ORM/Condition/Condition.cs
@@ -7,7 +7,7 @@ namespace DesignPattern_ORM
     abstract class Condition
     {
         // protected bool isNot = false;
-        public abstract string toSQL(Dictionary<string, string> featureMap);
+        public abstract string toSQL(Dictionary<string, string> featureMap, string aggFunc = "");
         /**
          USAGE
         
@@ -37,6 +37,5 @@ namespace DesignPattern_ORM
         {
             return new Equal(a, b);
         }
-        public 
     }
 }
